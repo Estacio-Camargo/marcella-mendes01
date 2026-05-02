ff5afcee60e267db66ddeabc49e753ee7d7a3eb1
sauron <git.github@outlook.comgit config user.email git.github@outlook.com>
2026-05-02 19:29:05 -0300

    corrigindo06

--- js/index.js
+++ js/index.js
@@ -265,7 +265,7 @@ function initLogin() {
             }
 
             loginScreen.style.display = 'none';
-            app.style.display = '';
+            app.classList.add('app-logged-in');
             errorMessage.textContent = '';
             // Rolar para o topo após login bem-sucedido
             window.scrollTo(0, 0);
